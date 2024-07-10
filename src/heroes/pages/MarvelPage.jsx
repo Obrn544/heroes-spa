5c5a3a378bcefc1068abd264abab9207462bdb32
Omar Obregon <oobregon544@gmail.com>
2024-07-09 23:07:22 -0500

    heroes-app

---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,11 @@
+import { HeroList } from '../components/HeroList';
+
+export function MarvelPage() {
+    return (
+        <>
+            <h1>Marvel Comics</h1>
+
+            <HeroList publisher='Marvel Comics' />
+        </>
+    );
+}
